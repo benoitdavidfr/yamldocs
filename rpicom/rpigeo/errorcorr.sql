9ee54d86b8a866d9af51304c75d9a7a0628f4964
Benoit DAVID <benoit.davi@free.fr>
2020-06-15 06:57:11 +0000

    rpigeo3 en cours, écriture de errorcorr.sql

--- rpicom/rpigeo/errorcorr.sql
+++ rpicom/rpigeo/errorcorr.sql
@@ -4,45 +4,48 @@ title: errorcorr.sql - vérifie la cohérence topologique et corrige les erreurs
 doc: |
   Ce script
     - vérifie que les commune_carto sont cohérentes topologiquement, cad
-      - elles ne s'intersectent entre elles que par des lignes ou des points
+      - leur intersection 2 à 2 correspond à des lignes ou des points
       - leur union couvre le territoire,
-        - pour cela on vérifie que seul FXX a une enclave dans son territoire, à savoir l'enclave espagnole de Llivia
+        - pour cela je vérifie que seul FXX a une enclave dans son territoire, à savoir l'enclave espagnole de Llivia
     - produit une couche corrigée des entités rattachées qui NE sont PAS cohérentes topologiquement avec commune_carto
-      - on vérifie que les entités rattachées ne s'intersectent pas entre elles
-      - on limite chaque entité rattachée à sa commune rattachante, 
-      - on calcule les entités complémentaires
-      - on identifie parmi elles celles qui sont des sliver que l'on agrège avec une entité voisine, pour cela
-        - on vérifie individuellement dans QGis les plus petites
+      - je vérifie que les entités rattachées ne s'intersectent pas entre elles
+      - je limite chaque entité rattachée à sa commune rattachante,
+      - je calcule les entités complémentaires
+      - j'identifie parmi elles celles qui sont des sliver (8) que j'agrège alors avec une entité voisine, pour cela
+        - je vérifie individuellement dans QGis les plus petites
+  Stratégie:
+    - je pars du principe que les communes simples sont correctes et je fonde donc les calculs sur elles
+    - je corrige les entités rattachées pour les mettre chacune en cohérence topologique avec leur commune de rattachement
   Tables en entrée:
-    - commune_carto
-    - entite_rattachee_carto
+    - commune_carto (AE2020COG)
+    - entite_rattachee_carto (AE2020COG)
   Tables en sortie:
     - eratcorrigee - Entités rattachées corrigées
     - ecomp - Entités complémentaires, cad complément éventuel des entités rattachées dans les c. rattachantes
-    - univers - Un rectangle englobant par grande zone géographique (FXX et chaque DOM)
+    - univers - Un rectangle englobant par zone géographique (FXX et chaque DOM)
     - comunionfxxdom - Union géométrique des communes, 1 pour FXX et 1 pour les DOM
-    - exterior - Extérieur pour chaque gde zone géo., cad MultiPolygone correspondant à la mer et l'étranger
+    - exterior - Extérieur pour chaque zone géo., cad Polygone/MultiPolygone correspondant à la mer et l'étranger
     - extfxx - Extérieur de FXX décomposé en 2 polygones, 1 pour l'extérieur et l'autre pour l'enclave espagnole de Llivia
-    - comint - Intersections entre commune_carto 2 à 2
-    - erint - Intersections entre entite_rattachee_carto 2 à 2
+  Tables temporaires:
+    - comint - intersection entre 2 commune_carto
+    - erint - intersectio entre 2 entite_rattachee_carto
     - srattache - union géométrique des entités rattachées groupées par rattachante
+  Termes de topologie:
+    - sliver
+    - overlap
 journal: |
   14/6/2020
     - première version
 */
-/*
-Topologie:
-- sliver
-- overlap
-*/
--- .sql - 
--- la stratégie est de se fonder sur les communes simples pour calculer l'extérieur
--- et de corriger les entitee_rattachee pour les mettre en cohérence avec leur rattachante
 
 --------------------------------
--- 1) Vérification des communes
+-- 1) Vérification de commune_carto
 --------------------------------
--- 1a) vérifier que les communes ne s'intersectent que comme ligne ou point
+-- 1a) Vérification de la validité des géométries
+select id from commune_carto where not ST_IsValid(wkb_geometry);
+
+-- 1a) vérifier que les commune_carto ne s'intersectent que comme ligne ou point
+drop table comint if exists;
 create table comint as
 select c1.id id1, c2.id id2, ST_Intersection(c1.wkb_geometry, c2.wkb_geometry) geom
 from commune_carto c1, commune_carto c2
@@ -56,17 +59,18 @@ where GeometryType(geom)<>'MULTILINESTRING'
   and GeometryType(geom)<>'LINESTRING'
   and GeometryType(geom)<>'POINT'
   and GeometryType(geom)<>'GEOMETRYCOLLECTION';
--- -> vide
+-- -> vide <=> OK
 
 -- si c'est une GEOMETRYCOLLECTION alors elle n'est composée que de POINT, LINESTRING ou MULTILINESTRING
 select id1, id2, numgeom, ST_GeometryN(geom, numgeom) geom
 from comint, generate_series(1,100) numgeom
 where GeometryType(geom)='GEOMETRYCOLLECTION'
-  and numgeom < ST_NumGeometries(geom)
+  and numgeom <= ST_NumGeometries(geom)
   and GeometryType(ST_GeometryN(geom, numgeom))<>'MULTILINESTRING'
   and GeometryType(ST_GeometryN(geom, numgeom))<>'LINESTRING'
   and GeometryType(ST_GeometryN(geom, numgeom))<>'POINT';
--- -> vide
+-- -> vide <=> OK
+drop table comint;
 
 -- 1b) vérifier que les extérieurs sont constitués pour les DOM d'un seul polygone (pas d'enclave)
 -- et pour la métropole d'un extérieur et du polygone correspondant à l'enclave de Llivia
@@ -110,7 +114,7 @@ union
   select num, iso3, ST_Difference(box, geom) as geom
   from univers, comunionfxxdom
   where iso3<>'FXX' and id='DOM';
-comment on table exterior is 'Extérieur pour chaque gde zone géo., cad MultiPolygone correspondant à la mer et l''étranger.';
+comment on table exterior is 'Extérieur pour chaque gde zone géo., cad Polygone/MultiPolygone correspondant à la mer et l''étranger.';
 
 select iso3, ST_AsText(geom) from exterior;
 
@@ -122,15 +126,20 @@ create table extfxx as
 -- contient 2 polygones, le 1er l'extérieur et le 2nd l'enclave espagnole de Llivia
 comment on table extfxx is 'Extérieur de FXX en 2 polygones, 1 pour l''extérieur et l''autre pour l''enclave espagnole de Llivia';
 
+-- le fait que l'extérieur de chaque DOM corresponde à 1 polygone
+-- et que l'extérieur de FXX corresponde à 2 polygones valide la cohérence la couverture de commune_carto
+
 ----------------------------------------
 -- 2) Correction des entités rattachées
 ----------------------------------------
+-- 2a) Vérification de la validité des géométries
+select id from entite_rattachee_carto where not ST_IsValid(wkb_geometry);
+
 -- 2a) vérifier que les entités rattachées ne s'intersectent que comme ligne ou point
 create table erint as
 select c1.id id1, c2.id id2, ST_Intersection(c1.wkb_geometry, c2.wkb_geometry) geom
 from entite_rattachee_carto c1, entite_rattachee_carto c2
 where c1.id < c2.id and c1.wkb_geometry && c2.wkb_geometry and ST_Intersects(c1.wkb_geometry, c2.wkb_geometry);
-comment on table erint is 'Intersections entre entite_rattachee_carto 2 à 2';
 
 -- l'intersection entre 2 er est soit un POINT, une LINESTRING, une MULTILINESTRING ou une GEOMETRYCOLLECTION
 select id1, id2, GeometryType(geom)
@@ -139,99 +148,168 @@ where GeometryType(geom)<>'MULTILINESTRING'
   and GeometryType(geom)<>'LINESTRING'
   and GeometryType(geom)<>'POINT'
   and GeometryType(geom)<>'GEOMETRYCOLLECTION';
--- -> vide
+-- -> vide OK
 
 -- si c'est une GEOMETRYCOLLECTION alors elle n'est composée que de POINT, LINESTRING ou MULTILINESTRING
 select id1, id2, numgeom, ST_GeometryN(geom, numgeom) geom
 from erint, generate_series(1,100) numgeom
 where GeometryType(geom)='GEOMETRYCOLLECTION'
-  and numgeom < ST_NumGeometries(geom)
+  and numgeom <= ST_NumGeometries(geom)
   and GeometryType(ST_GeometryN(geom, numgeom))<>'MULTILINESTRING'
   and GeometryType(ST_GeometryN(geom, numgeom))<>'LINESTRING'
   and GeometryType(ST_GeometryN(geom, numgeom))<>'POINT';
--- -> vide
+-- -> vide OK
+-- ca ne sert à rien de garder cette table puisque je corrige les entités rattachées
+drop table erint;
 
 -- 2b) chaque er doit être strictement incluse dans sa c. rattachante
--- er - crat est vide
+-- je contate que ce n'est pas le cas par la requête suivante qui liste les erreurs
 select er.id, ST_AsText(ST_Difference(er.wkb_geometry, c.wkb_geometry))
 from entite_rattachee_carto er, commune_carto c
 where er.insee_ratt=c.id
   and not ST_IsEmpty(ST_Difference(er.wkb_geometry, c.wkb_geometry));
 -- -> liste les erreurs
 
--- création d'une table des entités rattachées corrigées
+-- 2c) en conséquence je construis la table des entités rattachées corrigées en limitant chaque er à sa rattachante
 drop table if exists eratcorrigee;
 create table eratcorrigee as
 select er.ogc_fid, er.id, er.nom_com as nom, er.insee_ratt as crat, er.type, ST_Intersection(er.wkb_geometry, c.wkb_geometry) as geom
 from entite_rattachee_carto er, commune_carto c
 where er.insee_ratt=c.id;
-comment on table eratcorrigee is 'entités rattachées corrigées';
+comment on table eratcorrigee is 'Entités rattachées corrigées';
 create index eratcorrigee_geom_gist on eratcorrigee using gist(geom);
 
--- 1) créer les entités complémentaires (ecomp)
--- somme des entités rattachées groupées par rattachante
+-- 3) je crée les entités complémentaires (ecomp)
+-- 3a) somme des entités rattachées groupées par rattachante
 drop table if exists srattache;
 create table srattache as
   select crat as id, ST_Union(geom) as geom
   from eratcorrigee
   group by crat;
 
--- calcul des entités complémentaires éventuelles (416)
+-- 3b) calcul des entités complémentaires éventuelles (416)
 -- l'id est le code INSEE concaténé avec 'c'
 drop table if exists ecomp;
 create table ecomp as
-  select concat(c.id, 'c') id, c.id crat, ST_Difference(c.wkb_geometry, sr.geom) geom
+  select concat(c.id, 'c') id, c.id crat, 0 npol, ST_Difference(c.wkb_geometry, sr.geom) geom
   from commune_carto c, srattache sr
   where c.id=sr.id and not ST_IsEmpty(ST_Difference(c.wkb_geometry, sr.geom));
-  
-select id, ST_Area(geom)*40000*40000/360/360 as areaKm2
+comment on table ecomp is 'Entités complémentaires, cad complément éventuel des entités rattachées dans leur c. de rattachement';
+drop table srattache;
+
+-- décomposition des ecomp MULTIPOLYGON
+insert into ecomp
+select id, crat, npol2, ST_GeometryN(geom, npol2) geom
+from ecomp, generate_series(1,1000) npol2
+where GeometryType(geom)='MULTIPOLYGON' and npol2 <= ST_NumGeometries(geom);
+
+delete from ecomp where GeometryType(geom)='MULTIPOLYGON' and npol=0;
+
+select id, crat, npol, ST_AsText(geom) from ecomppol;
+
+-- calcul de leur surface en km2 et affichage des plus petites pour détecter les slivers
+select id, crat, npol, ST_Area(geom)*40000*40000/360/360 as areaKm2
 from ecomp
 order by ST_Area(geom)*40000*40000/360/360; 
 
--- 52064 = (52064 + 52064c)
+id	crat	npol	areakm2
+x52064c	52064	2 6.33212822347475e-17
+x27467c	27467	2 1.79046817824318e-15
+x08173c	08173	2 7.7013832419803e-15
+x08173c	08173	4 1.94885801546187e-05
+x49228c	49228	1 3.37163552848493e-05
+x43090c	43090	2 5.43464591717906e-05
+x08173c	08173	1 9.11016908560262e-05
+x52064c	52064	1 0.000132289599242727
+x52064c	52064	5 0.000262799429442942
+x52064c	52064	4 0.000272037110280213
+x52064c	52064	3 0.000326436883426846
+x28103c	28103	2 0.000809999814823188
+x08173c	08173	3 0.00125505290728051
+x28103c	28103	3 0.00143883246912925
+x27467c	27467	1 0.0020087713971907
+x28103c	28103	1 0.00219803037034413
+x14431c	14431	0 0.00509415567895506
+x52008c	52008	2 0.00705760477756044
+x72137c	72137	0 0.00810781790124743
+x49228c	49228	2 0.0143040400001143
+x48105c	48105	0 0.115628045061655
+x52400c	52400	2 0.160525998209755
+08362c	08362	1 0.170180754444408 ok
+45253c	45253	2 2.23703171851859 ok
+14515c	14515	0 2.24770399475292 ok
+
+-- après analyse visuelle individuelle sous QGis, suppression de 8 ecomp qui sont des slivers et ajout de leur territoire à un erat
+-- correction: 52064 = (52064 + 52064c) / 1-5
 update eratcorrigee
-  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='52064' and c.id='52064c')
+  set geom=ST_Union((select geom from eratcorrigee where id='52064'), (select ST_Union(geom) from ecomp where id='52064c' group by id))
   where id='52064';
 delete from ecomp where id='52064c';
 
--- solution: 08079 = (08079 + 08173c)
+-- correction: 27467 = (27467 + 27467c)
 update eratcorrigee
-  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='08079' and c.id='08173c')
-  where id='08079';
-delete from ecomp where id='08173c';
-
--- solution: 27467 = (27467 + 27467c)
-update eratcorrigee
-  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='27467' and c.id='27467c')
+  set geom=ST_Union((select geom from eratcorrigee where id='27467'), (select ST_Union(geom) from ecomp where id='27467c' group by id))
   where id='27467';
 delete from ecomp where id='27467c';
 
--- solution: 28262 = (28262 + 28103c)
-update eratcorrigee
-  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='28262' and c.id='28103c')
-  where id='28262';
-delete from ecomp where id='28103c';
+  A LANCER
 
--- solution: 14201 = (14201 + 14431c)
+-- correction: 08079 = (08079 + 08173c)
 update eratcorrigee
-  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='14201' and c.id='14431c')
-  where id='14201';
-delete from ecomp where id='14431c';
+  set geom=ST_Union((select geom from eratcorrigee where id='08079'), (select ST_Union(geom) from ecomp where id='08173c' group by id))
+  where id='08079';
+delete from ecomp where id='08173c';
 
--- solution: 72137 = (72137 + 72137c)
+-- correction: 49103 = (49103 + 49228c)
 update eratcorrigee
-  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='72137' and c.id='72137c')
-  where id='72137';
-delete from ecomp where id='72137c';
-
--- solution: 49103 = (49103 + 49228c)
-update eratcorrigee
-  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='49103' and c.id='49228c')
+  set geom=ST_Union((select geom from eratcorrigee where id='49103'), (select ST_Union(geom) from ecomp where id='49228c' group by id))
   where id='49103';
 delete from ecomp where id='49228c';
 
--- solution: 48105 = (48105 + 48105c)
+-- correction: 43255 = (43255 + 43090c/2)
 update eratcorrigee
-  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='48105' and c.id='48105c')
+  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='43255' and c.id='43090c' and npol=2)
+  where id='43255';
+delete from ecomp where id='43090c' and npol=2;
+
+-- correction: 28262 = (28262 + 28103c)
+update eratcorrigee
+  set geom=ST_Union((select geom from eratcorrigee where id='28262'), (select ST_Union(geom) from ecomp where id='28103c' group by id))
+  where id='28262';
+delete from ecomp where id='28103c';
+
+-- correction: 14201 = (14201 + 14431c)
+update eratcorrigee
+  set geom=ST_Union((select geom from eratcorrigee where id='14201'), (select ST_Union(geom) from ecomp where id='14431c' group by id))
+  where id='14201';
+delete from ecomp where id='14431c';
+
+-- correction: 52054 = (52054 + 52008c/2)
+update eratcorrigee
+  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='52054' and c.id='52008c' and npol=2)
+  where id='52054';
+delete from ecomp where id='52008c' and npol=2;
+
+-- correction: 72137 = (72137 + 72137c)
+update eratcorrigee
+  set geom=ST_Union((select geom from eratcorrigee where id='72137'), (select ST_Union(geom) from ecomp where id='72137c' group by id))
+  where id='72137';
+delete from ecomp where id='72137c';
+
+-- correction: 49013 = (49013 + 49228c)
+update eratcorrigee
+  set geom=ST_Union((select geom from eratcorrigee where id='49013'), (select ST_Union(geom) from ecomp where id='49228c' group by id))
+  where id='49013';
+delete from ecomp where id='49228c';
+  
+-- correction: 48105 = (48105 + 48105c)
+update eratcorrigee
+  set geom=ST_Union((select geom from eratcorrigee where id='48105'), (select ST_Union(geom) from ecomp where id='48105c' group by id))
   where id='48105';
 delete from ecomp where id='48105c';
+
+-- correction: 52041 = (52041 + 52400c/2)
+update eratcorrigee
+  set geom=(select ST_Union(r.geom, c.geom) from eratcorrigee r, ecomp c where r.id='52041' and c.id='52400c' and npol=2)
+  where id='52041';
+delete from ecomp where id='52400c' and npol=2;
--- rpicom/rpigeo/rpigeo3.sql
+++ rpicom/rpigeo/rpigeo3.sql
@@ -14,7 +14,7 @@ doc: |
 
   Dans un premier temps on constitue les données initiales à partir des couches commune_carto et entite_rattachee_carto de AE2020COG
   corrigées dans ~/html/data/aegeofla/makefra.php des bugs initiaux détectées
-  L'étape préliminaire consiste à vérifier les données IGN et éventuellement les corriger.
+  L'étape préliminaire consiste à vérifier les données IGN et éventuellement les corriger dans errorcorr.sql.
 
   L'algorithme est ensuite le suivant:
     1) fabriquer une couche d'éléments (elt) correspondant à un pavage du territoire, cad ne s'intersectant pas 2 à 2
@@ -24,58 +24,57 @@ doc: |
       c) les parties des c. simples non rattachantes non couvertes par des e. rattachées, appelées e. complémentaires
       Les éléments correspondent aux faces du graphe topologique administratif fusionnant les communes simples et les e. rattachées.
     2) générer les limites entre ces éléments en calculant leur intersection 2 à 2
-    3) générer les limites extérieures des éléments - voir exterior.sql
+    3) générer les limites extérieures des éléments - voir exterior3.sql
     4) peupler lim et eadmvlim à partir des 2 ens. de limites précédemment constitués
     5) calculer les limites des c. rattachantes et les ajouter à eadmvlim
 
   Pour que le traitement ne génère pas d'erreurs, il faut définir les pré-conditions et les tester.
   En l'espèce, la pré-condition est que les éléments + l'extérieur constituent un pavage de l'univers.
-  Cela peut se traduire par:
-    1) vérifier que 2 éléments ne s'intersectent pas
-    2) construire l'extérieur à partir des éléments
+  Le test est effectué dans errorcorr.sql qui génère plusieurs tables réutilisées par la suite
+
+  Tables en entrée:
+    - commune_carto
+    - eratcorrigee - prduit dans errorcorr.sql
+    - ecomp - prduit dans errorcorr.sql
 journal: |
+  15/6/2020
+    8:50
+      - reste au moins 3 erreurs dans la construction des limites des elts
+        Erreurs:
+         - 49013 COMD de 49228
+         - 52054 COMD de 52008
+         - 65116 COMS
+      - reprendre le code rpigeo2.sql pour la suite
+  14/6/2020
+    - écriture de errorcorr.sql qui teste la pré-condition et effectue des corrections nécessaires
+    - écriture de errorcorrsup.sql qui rédéfinit qqs éléments qui posent problèmes et que je ne sais pas corriger autrement
   13/6/2020
     - suite de rpigeo2.sql
     - utilise schema.sql
 */
 
+----------------------------------------------------------------------------------
+-- 0) tester les pré-conditions et corriger les données en entrée -> errorcor.sql
+----------------------------------------------------------------------------------
+
 ---------------------------------------
--- 1) fabriquer la couche des éléments
+-- 1) fabriquer la table des éléments
 ---------------------------------------
--- 1a) créer les entités complémentaires (ecomp) - somme des entités rattachées groupées par rattachante
--- 1a.i) somme des rattachées (1076)
-drop table if exists srattache;
-create table srattache as
-  select insee_ratt as cinsee, ST_Union(wkb_geometry) as geom
-  from entite_rattachee_carto
-  group by insee_ratt;
-
--- calcul des entités complémentaires éventuelles (416)
--- l'id est le code INSEE concaténé avec 'c', cela évite qu'une entité complémentaire porte un code INSEE existant
-drop table if exists ecomp;
-create table ecomp as
-  select concat(c.id, 'c') id, c.id insee_ratt, ST_Difference(c.wkb_geometry, sr.geom) geom
-  from commune_carto c, srattache sr
-  where c.id=sr.cinsee and ST_Dimension(ST_Difference(c.wkb_geometry, sr.geom))=2;
-
--- affichage
-select id, ST_Dimension(geom), ST_AsText(geom) from ecomp;
-
--- 1b) fabrication de la couche des éléments en substituant aux c. rattachantes leurs entités rattachées + complémentaires.
--- + l'extérieur (37239)
+-- 1) fabrication de la table des éléments en substituant aux c. rattachantes leurs entités rattachées + complémentaires.
 -- schema: id, type, geom
 drop table if exists elt;
 create table elt as
   -- les c. s. non rattachantes
   select id, 'cSimple' as type, wkb_geometry as geom
   from commune_carto
-  where id not in (select insee_ratt from entite_rattachee_carto)
+  where id not in (select crat from eratcorrigee)
 union
   -- les e. rattachées / type vaut COMA, COMD ou ARM
-  select id, type, wkb_geometry as geom from entite_rattachee_carto
+  select id, type, geom from eratcorrigee
 union
   -- les complémentaires
   select id, 'ec' as type, geom from ecomp;
+comment on table elt is 'Elément administif = c. simples non ratt. + entités rattachées + entités complémentaires';
 create index elt_geom_gist on elt using gist(geom);
 
 ----------------------------------------------------------------------------------
@@ -83,63 +82,39 @@ create index elt_geom_gist on elt using gist(geom);
 ----------------------------------------------------------------------------------
 -- 2a) calcul des intersections entre éléments en supprimant les intersections vides ou réduites à un point
 -- 109142 - prend 4'10" sur Mac
-drop table if exists eltelt;
+drop table if exists eltint;
 select 'Début:', now();
-create table eltelt as 
+create table eltint as 
 select e1.id id1, e1.type typ1, e2.id id2, e2.type typ2, ST_Intersection(e1.geom, e2.geom) geom
 from elt e1, elt e2
 where e1.geom && e2.geom and e1.id < e2.id and ST_Dimension(ST_Intersection(e1.geom, e2.geom)) > 0;
 select 'Fin:', now();
-create index eltelt_geom_gist on eltelt using gist(geom);
+create index eltint_geom_gist on eltint using gist(geom);
 
 -- 2b) liste des intersections non linéaires, fait partie des tests de pré-condition
--- 16 tuples générés
-select id1, typ1, id2, typ2, ST_Dimension(geom), ST_AsText(geom) from eltelt where ST_Dimension(geom)=1;
+select id1, typ1, id2, typ2, ST_Dimension(geom), ST_AsText(geom) from eltint where ST_Dimension(geom)<>1;
 
--- 465
-drop table if exists eltelterror;
-create table eltelterror as
+-- 243
+drop table if exists eltinterror;
+create table eltinterror as
 select id1, typ1, id2, typ2, numgeom, ST_GeometryN(geom, numgeom) geom
-from eltelt, generate_series(1,1000) numgeom
-where ST_Dimension(geom)<>1 and numgeom < ST_NumGeometries(geom);
+from eltint, generate_series(1,1000) numgeom
+where ST_Dimension(geom)<>1 and numgeom <= ST_NumGeometries(geom);
 
-select id1, typ1, id2, typ2, numgeom, ST_AsText(geom) from eltelterror where ST_Dimension(geom)=2;
+select id1, typ1, id2, typ2, numgeom, ST_AsText(geom) from eltinterror where ST_Dimension(geom)=2;
+select id1, typ1, id2, typ2, numgeom, ST_AsGeoJSON(geom) from eltinterror where ST_Dimension(geom)=2;
 
+-- vide
 
--- corrections dues aux bugs d'incohérence topologique sur les données IGN
--- erreur d'incohérence entre 08079 (COMD de 08173) et 08400
--- solution: 08079 = (08079 + 08173c) - 08400
-update elt
-  set geom=(select ST_Union(e1.geom, e2.geom) from elt e1, elt e2 where e1.id='08079' and e2.id='08173c')
-  where id='08079';
-update elt
-  set geom=(select ST_Difference(e1.geom, e2.geom) from elt e1, elt e2 where e1.id='08079' and e2.id='08400')
-  where id='08079';
-delete from elt where id='08173c';
+------------------------------------------------------
+-- 3) générer les limites extérieures -> exterior3.sql
+------------------------------------------------------
 
--- erreur d'incohérence entre 52064 (COMD de 52064) et 52455
--- 52064 = (52064 + 52064c) - 52455
-update elt
-  set geom=(select ST_Union(e1.geom, e2.geom) from elt e1, elt e2 where e1.id='52064' and e2.id='52064c')
-  where id='52064';
-update elt
-  set geom=(select ST_Difference(e1.geom, e2.geom) from elt e1, elt e2 where e1.id='52064' and e2.id='52455')
-  where id='52064';
-delete from elt where id='52064c';
+Analyse visuelle pour vérifier que eltint + eltextlim contiennent les limites de commune_carteo et eratcorrigee
 
--- 52054 = (52054 + 52008c) - 52107
-update elt
-  set geom=(select ST_Union(e1.geom, e2.geom) from elt e1, elt e2 where e1.id='52054' and e2.id='52008c')
-  where id='52054';
-update elt
-  set geom=(select ST_Difference(e1.geom, e2.geom) from elt e1, elt e2 where e1.id='52054' and e2.id='52107')
-  where id='52054';
-delete from elt where id='52008c';
+Erreurs:
+ - 49013 COMD de 49228
+ - 52054 COMD de 52008
+ - 65116 COMS
 
--- 43255 = 43255 - 43245
-update elt
-  set geom=(select ST_Difference(e1.geom, e2.geom) from elt e1, elt e2 where e1.id='43255' and e2.id='43245')
-  where id='43255';
-
-Puis construire l'extérieur à partir des elts pour assurer la cohérence
 
